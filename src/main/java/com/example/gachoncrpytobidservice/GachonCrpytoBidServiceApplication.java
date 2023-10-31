dd830ac395ec47389ab92c3c16171189d3aa50b8
RyuKwanKon <rhks33rhs@gmail.com>
2023-11-01 00:28:09 +0900

    [init] project 생성

---
+++ src/main/java/com/example/gachoncrpytobidservice/GachonCrpytoBidServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.gachoncrpytobidservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GachonCrpytoBidServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GachonCrpytoBidServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gachoncrpytobidservice/GachonCrpytoBidServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gachoncrpytobidservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GachonCrpytoBidServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
