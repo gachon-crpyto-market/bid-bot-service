dc868ec1af1294dbb846cc827e930f71cd66f73c
RyuKwanKon <rhks33rhs@gmail.com>
2023-11-01 02:19:49 +0900

    [feat] 1초 단위 봇 스케쥴러 구현

---
+++ src/main/java/com/example/gachoncrpytobidservice/scheduler/BidScheduler.java
@@ -0,0 +1,26 @@
+package com.example.gachoncrpytobidservice.scheduler;
+
+import com.example.gachoncrpytobidservice.service.BidService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+@EnableAsync
+@EnableScheduling
+public class BidScheduler {
+    private final BidService bidService;
+    @Scheduled(fixedDelay = 1000)
+    public void bidScheduleTask() {
+        System.out.println(bidService.getCurrentBidPrice());
+    }
+
+
+}
